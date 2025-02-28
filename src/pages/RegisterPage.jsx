e3b75fdfd1c483c4db5d97fb1a1a93006de4319c
Fajar Nazmi Fadillah <legion@telkom.design>
2025-02-28 14:14:32 +0700

    feature; render smaller heading in Login and Register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -19,7 +19,7 @@ const RegisterPage = () => {
 
   return (
     <section className="grid grid-cols-1 lg:grid-cols-[1.25fr_1fr] min-h-screen">
-      <header className="flex items-center justify-center text-9xl bg-amber-500">
+      <header className="flex items-center justify-center text-7xl lg:text-9xl bg-amber-500">
         <h1>Rumpi.</h1>
       </header>
       <article className="flex gap-4 justify-center flex-col pt-0 px-8 lg:p-16 bg-gray-100 dark:text-white dark:bg-gray-800">
